bfec59deccf2763c07e6dd0da387614f6765b0b9
glebite <glebite@gmail.com>
2023-01-25 20:42:27 -0500

    CRLF in text

--- src/ursina_button.py
+++ src/ursina_button.py
@@ -1,9 +1,11 @@
 from ursina import *
 
+
 app = Ursina()
 
-button = Button(text='Thing', color=color.blue, scale = 0.5)
 
+# button1 = Button(text='Thing', color=color.blue, scale=0.5)
+button2 = Button(text='Other\nThing', color=color.red, scale=0.5)
 
 def update():
     pass
