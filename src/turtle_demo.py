368cef409fb844cd39e861178c391d3e992e3101
glebite <glebite@gmail.com>
2023-02-19 20:26:30 -0500

    Cleaning up - thinking more thoughts.

--- src/turtle_demo.py
+++ src/turtle_demo.py
@@ -1,11 +1,20 @@
-from turtle import *
-color('red', 'yellow')
-begin_fill()
-while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
-        break
-end_fill()
-done()
+""" turtle_demo.py
 
+"""
+import turtle as t
+
+
+def draw_star(distance: int, angle: int) -> None:
+    t.color('red', 'yellow')
+    t.begin_fill()
+    while True:
+        t.forward(distance)
+        t.left(angle)
+        if abs(t.pos()) < 1:
+            break
+    t.end_fill()
+    t.done()
+
+
+if __name__ == "__main__":
+    draw_star(200, 170)
